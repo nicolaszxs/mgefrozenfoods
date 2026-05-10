694183dc98713999e034a722016f5f03a8ce83a9
psalud8955-wq <psalud.8955@umak.edu.ph>
2026-05-10 20:08:21 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/mgefrozenfoods/CarouselAdapter.java
@@ -0,0 +1,42 @@
+package com.example.mgefrozenfoods;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.List;
+
+public class CarouselAdapter extends RecyclerView.Adapter<CarouselAdapter.ViewHolder> {
+    private List<Integer> imageList;
+
+    public CarouselAdapter(List<Integer> imageList) {
+        this.imageList = imageList;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_carousel, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.imageView.setImageResource(imageList.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return imageList.size();
+    }
+
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        ImageView imageView;
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            imageView = itemView.findViewById(R.id.ivCarouselImage);
+        }
+    }
+}
