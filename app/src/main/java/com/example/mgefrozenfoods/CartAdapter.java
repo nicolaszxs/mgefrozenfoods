d8b935ff4d63df90e783cc0ae88fafe1e86460bf
Angelico Nicdao <anicdao.8903@umak.edu.ph>
2026-05-15 14:01:57 +0800

    CartAdapter.java

--- app/src/main/java/com/example/mgefrozenfoods/CartAdapter.java
+++ app/src/main/java/com/example/mgefrozenfoods/CartAdapter.java
@@ -1,7 +1,6 @@
 package com.example.mgefrozenfoods;
 
-import android.text.Editable;
-import android.text.TextWatcher;
+
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -13,82 +12,92 @@ import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 import java.util.List;
 
+
 public class CartAdapter extends RecyclerView.Adapter<CartAdapter.CartViewHolder> {
 
-    private List<CartItem> cartItemList;
-    private CartListener listener;
 
-    public interface CartListener {
-        void onRemoveItem(int position);
-    }
+   private List<CartItem> cartItemList;
+   private CartListener listener;
 
-    public CartAdapter(List<CartItem> cartItemList, CartListener listener) {
-        this.cartItemList = cartItemList;
-        this.listener = listener;
-    }
 
-    @NonNull
-    @Override
-    public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart, parent, false);
-        return new CartViewHolder(view);
-    }
+   public interface CartListener {
+       void onRemoveItem(int position);
+       void onQuantityChanged();
+   }
 
-    @Override
-    public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
-        CartItem item = cartItemList.get(position);
 
-        holder.tvCartName.setText(item.getProduct().getName());
-        holder.tvCartPrice.setText(item.getProduct().getPrice());
-        holder.ivCartImage.setImageResource(item.getProduct().getImageResId());
+   public CartAdapter(List<CartItem> cartItemList, CartListener listener) {
+       this.cartItemList = cartItemList;
+       this.listener = listener;
+   }
 
-        holder.etCartQuantity.setText(String.valueOf(item.getQuantity()));
-        holder.tvCartSubtotal.setText(String.format("₱%.2f", item.getSubtotal()));
 
-        holder.etCartQuantity.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
+   @NonNull
+   @Override
+   public CartViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+       View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_cart, parent, false);
+       return new CartViewHolder(view);
+   }
 
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                try {
-                    int qty = Integer.parseInt(s.toString());
-                    if (qty > 0) {
-                        item.setQuantity(qty);
-                    }
-                } catch (Exception e) {}
-            }
 
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
+   @Override
+   public void onBindViewHolder(@NonNull CartViewHolder holder, int position) {
+       CartItem item = cartItemList.get(position);
 
-        holder.btnRemoveCart.setOnClickListener(v -> {
-            if (listener != null) {
-                listener.onRemoveItem(position);
-            }
-        });
-    }
 
-    @Override
-    public int getItemCount() {
-        return cartItemList.size();
-    }
+       holder.tvCartName.setText(item.getProduct().getName());
+       holder.tvCartPrice.setText(item.getProduct().getPrice());
+       holder.ivCartImage.setImageResource(item.getProduct().getImageResId());
 
-    public static class CartViewHolder extends RecyclerView.ViewHolder {
-        ImageView ivCartImage;
-        TextView tvCartName, tvCartPrice, tvCartSubtotal;
-        EditText etCartQuantity;
-        Button btnRemoveCart;
 
-        public CartViewHolder(@NonNull View itemView) {
-            super(itemView);
-            ivCartImage = itemView.findViewById(R.id.ivCartImage);
-            tvCartName = itemView.findViewById(R.id.tvCartName);
-            tvCartPrice = itemView.findViewById(R.id.tvCartPrice);
-            tvCartSubtotal = itemView.findViewById(R.id.tvCartSubtotal);
-            etCartQuantity = itemView.findViewById(R.id.etCartQuantity);
-            btnRemoveCart = itemView.findViewById(R.id.btnRemoveCart);
-        }
-    }
+       holder.etCartQuantity.setText(String.valueOf(item.getQuantity()));
+       holder.tvCartSubtotal.setText(String.format("₱%.2f", item.getSubtotal()));
+
+
+       holder.btnUpdateCart.setOnClickListener(v -> {
+           try {
+               int qty = Integer.parseInt(holder.etCartQuantity.getText().toString());
+               if (qty > 0) {
+                   item.setQuantity(qty);
+                   holder.tvCartSubtotal.setText(String.format("₱%.2f", item.getSubtotal()));
+                   if (listener != null) {
+                       listener.onQuantityChanged();
+                   }
+               }
+           } catch (Exception e) {}
+       });
+
+
+       holder.btnRemoveCart.setOnClickListener(v -> {
+           if (listener != null) {
+               listener.onRemoveItem(position);
+           }
+       });
+   }
+
+
+   @Override
+   public int getItemCount() {
+       return cartItemList.size();
+   }
+
+
+   public static class CartViewHolder extends RecyclerView.ViewHolder {
+       ImageView ivCartImage;
+       TextView tvCartName, tvCartPrice, tvCartSubtotal;
+       EditText etCartQuantity;
+       Button btnRemoveCart, btnUpdateCart;
+
+
+       public CartViewHolder(@NonNull View itemView) {
+           super(itemView);
+           ivCartImage = itemView.findViewById(R.id.ivCartImage);
+           tvCartName = itemView.findViewById(R.id.tvCartName);
+           tvCartPrice = itemView.findViewById(R.id.tvCartPrice);
+           tvCartSubtotal = itemView.findViewById(R.id.tvCartSubtotal);
+           etCartQuantity = itemView.findViewById(R.id.etCartQuantity);
+           btnRemoveCart = itemView.findViewById(R.id.btnRemoveCart);
+           btnUpdateCart = itemView.findViewById(R.id.btnUpdateCart);
+       }
+   }
 }
